2e680b96f7e759ae2a44c86ccd50dc1ad996bc5e
alabama3425 <70514474+hadgehog666@users.noreply.github.com>
2025-12-21 21:19:58 +0500

    2
    Написана вёрстка части страниц

---
+++ HeatExchangeCalculator/Views/HeatExchange/SavedCalculations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeatExchangeCalculator.Views.HeatExchange
+{
+    public class SavedCalculationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
